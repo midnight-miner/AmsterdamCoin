965cab7f42cdda266caf40b65da2093254fe9f7b
joshafest <joshaf.est@gmail.com>
2015-10-07 22:02:33 +0530

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2281,14 +2281,15 @@ bool CBlock::AcceptBlock(CValidationState &state, CDiskBlockPos *dbp)
 }
 
 bool CBlockIndex::IsSuperMajority(int minVersion, const CBlockIndex* pstart, unsigned int nRequired, unsigned int nToCheck)
-{
-    unsigned int nFound = 0;
-    for (unsigned int i = 0; i < nToCheck && nFound < nRequired && pstart != NULL; i++)
-    {
-        if (pstart->nVersion >= minVersion)
-            ++nFound;
-        pstart = pstart->pprev;
-    }
+{        // temporarily disable v2 block lockin until ready for v2 transition
+     return false;
+     unsigned int nFound = 0;
+     for (unsigned int i = 0; i < nToCheck && nFound < nRequired && pstart != NULL; i++)
+     {
+       if (pstart->nVersion >= minVersion)
+             ++nFound;
+       pstart = pstart->pprev;
+      }
     return (nFound >= nRequired);
 }
 
