488d14ab4ec1e6703dd6ee47bd55551c747bb986
joshafest <joshaf.est@gmail.com>
2015-10-07 22:48:50 +0530

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2218,38 +2218,29 @@ bool CBlock::AcceptBlock(CValidationState &state, CDiskBlockPos *dbp)
         if (pcheckpoint && nHeight < pcheckpoint->nHeight)
             return state.DoS(100, error("AcceptBlock() : forked chain older than last checkpoint (height %d)", nHeight));
 
-        // Reject block.nVersion=1 blocks (mainnet >= 710000, testnet >= 400000)
-        if (nVersion < 2)
-        {if ((!fTestNet && CBlockIndex::IsSuperMajority(2, pindexPrev, 750, 1000)) ||
- (fTestNet && CBlockIndex::IsSuperMajority(2, pindexPrev, 51, 100)))
-            {
-                return state.Invalid(error("AcceptBlock() : rejected nVersion=1 block"));
-            }
+        // Reject block.nVersion=1 or =2 blocks when 95% (75% on testnet) of the network has upgraded:
+        if (nVersion < 3)
+        {
+          if ((!fTestNet && CBlockIndex::IsSuperMajority(3, pindexPrev, 950, 1000)) ||
+              (fTestNet && CBlockIndex::IsSuperMajority(3, pindexPrev, 75, 100)))
+          {
+               return state.Invalid(error("AcceptBlock() : rejected old nVersion block"));
+          }
         }
         // Enforce block.nVersion=2 rule that the coinbase starts with serialized block height
-        if (nVersion == 2)
-        {
-           if ((!fTestNet && CBlockIndex::IsSuperMajority(2, pindexPrev, 750, 1000)) ||
- (fTestNet && CBlockIndex::IsSuperMajority(2, pindexPrev, 51, 100)))
-            {
-                CScript expect = CScript() << nHeight;
-                if (vtx[0].vin[0].scriptSig.size() < expect.size() ||
-                    !std::equal(expect.begin(), expect.end(), vtx[0].vin[0].scriptSig.begin()))
-                    return state.DoS(100, error("AcceptBlock() : block height mismatch in coinbase"));
+        if (nVersion >= 2)
+           {
+                // if 750 of the last 1,000 blocks are version 2 or greater (51/100 if testnet):
+                if ((!fTestNet && CBlockIndex::IsSuperMajority(2, pindexPrev, 750, 1000)) ||
+                    (fTestNet && CBlockIndex::IsSuperMajority(2, pindexPrev, 51, 100)))
+                 {
+                    CScript expect = CScript() << nHeight;
+                    if (vtx[0].vin[0].scriptSig.size() < expect.size() ||
+                       !std::equal(expect.begin(), expect.end(), vtx[0].vin[0].scriptSig.begin()))
+                        return state.DoS(100, error("AcceptBlock() : block height mismatch in coinbase"));
+                 }
             }
-         if (nVersion >= 3)
- {
- if ((!fTestNet && CBlockIndex::IsSuperMajority(3, pindexPrev, 750, 1000)) ||
- (fTestNet && CBlockIndex::IsSuperMajority(3, pindexPrev, 51, 100)))
- {
- CScript expect = CScript() << nHeight;
- if (vtx[0].vin[0].scriptSig.size() < expect.size() ||
- !std::equal(expect.begin(), expect.end(), vtx[0].vin[0].scriptSig.begin()))
- return state.DoS(100, error("AcceptBlock() : block height mismatch in coinbase"));
-}
-        }
-    }
-
+     }
     // Write block to history file
     try {
         unsigned int nBlockSize = ::GetSerializeSize(*this, SER_DISK, CLIENT_VERSION);
